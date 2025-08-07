25b08e210cf1d77dd68544835f9443607f3daff5
jdurangen <jduran@genit.com.ar>
2025-08-06 22:19:00 -0300

    Initial Commit

---
+++ note-debugger/note_debuggerApp.swift
@@ -0,0 +1,32 @@
+//
+//  note_debuggerApp.swift
+//  note-debugger
+//
+//  Created by Jonathan Duran on 06/08/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct note_debuggerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
