d78bd92d6c79962718c15396fd15e71f019140e8
jonathanduran7 <joniduran624@gmail.com>
2025-08-06 23:52:54 -0300

    Enable network logging in debug mode and initialize Pulse components in note_debuggerApp.swift

--- note-debugger/note_debuggerApp.swift
+++ note-debugger/note_debuggerApp.swift
@@ -6,9 +6,25 @@
 //
 
 import SwiftUI
+import Pulse
+import PulseUI
+
+#if DEBUG
+import PulseProxy
+#endif
 
 @main
 struct note_debuggerApp: App {
+    
+    init() {
+        #if DEBUG
+            print("DEBUG")
+            NetworkLogger.enableProxy()
+//            let session: URLSessionProtocol = URLSessionProxy(configuration: .default)
+            RemoteLogger.shared.isAutomaticConnectionEnabled = true
+        #endif
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
