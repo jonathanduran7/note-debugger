39b3868198f3f7a9bf219fd40cc4ba666c820823
jonathanduran7 <joniduran624@gmail.com>
2025-08-06 23:03:49 -0300

    add theme

---
+++ note-debugger/ColorPalette.swift
@@ -0,0 +1,42 @@
+//
+//  ColorPalette.swift
+//  note-debugger
+//
+//  Created by Jonathan Duran on 06/08/2025.
+//
+
+import SwiftUI
+
+struct ColorPalette {
+    static let primary = Color(hex: "1A2A80")      // Azul oscuro
+    static let secondary = Color(hex: "3B38A0")    // Azul medio
+    static let accent = Color(hex: "7A85C1")       // Azul claro
+    static let light = Color(hex: "B2B0E8")        // Azul muy claro
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
